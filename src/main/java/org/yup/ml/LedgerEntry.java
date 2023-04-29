93ba47b78f32684e6cdef13a33359136142176aa
iconbaypark2900 <jonaston015@gmail.com>
2023-04-29 11:19:28 -0400

    probably bad idea

---
+++ src/main/java/org/yup/ml/LedgerEntry.java
@@ -0,0 +1,12 @@
+package org.yup.ml;
+
+import java.util.Date;
+
+public class LedgerEntry {
+    private Date date;
+    private String description;
+    private String vendor;
+    private double amount;
+    private boolean isDeposit;
+
+}
---
+++ src/main/java/org/yup/ml/Reports.java
@@ -0,0 +1,4 @@
+package org.yup.ml;
+
+public class Reports {
+}
