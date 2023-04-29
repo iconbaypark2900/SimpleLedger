a63b28be473ef654398e6c3d04112dcec531eabf
iconbaypark2900 <jonaston015@gmail.com>
2023-04-29 16:35:31 -0400

    making methods for the ledger class

--- src/main/java/org/yup/ml/Ledger.java
+++ src/main/java/org/yup/ml/Ledger.java
@@ -1,4 +1,22 @@
 package org.yup.ml;
 
+import java.util.Date;
+import java.util.List;
+
 public class Ledger {
+    private List<LedgerEntry> entries;
+
+    public void addEntry(LedgerEntry entry) {
+        // Code to add a new entry to the ledger
+    }
+
+    public void displayEntries(boolean showDeposits, boolean showPayments) {
+        // Code to display entries based on the input parameters
+    }
+
+    public List<LedgerEntry> filterEntries(Date startDate, Date endDate, String description, String vendor, Double amount) {
+        return null;
+    }
+
+
 }
--- src/main/java/org/yup/ml/LedgerEntry.java
+++ src/main/java/org/yup/ml/LedgerEntry.java
@@ -9,4 +9,47 @@ public class LedgerEntry {
     private double amount;
     private boolean isDeposit;
 
+    public LedgerEntry(Date date, String description, String vendor, double amount) {
+        this.date = date;
+        this.description = description;
+        this.vendor = vendor;
+        this.amount = amount;
+        this.isDeposit = isDeposit();
+    }
+
+    private boolean isDeposit() {
+        return amount >= 0;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getVendor() {
+        return vendor;
+    }
+
+    public void setVendor(String vendor) {
+        this.vendor = vendor;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
 }
