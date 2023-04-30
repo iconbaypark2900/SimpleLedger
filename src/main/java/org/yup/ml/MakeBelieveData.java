788caf3a6a57496177631e08410e59e23b6844df
iconbaypark2900 <jonaston015@gmail.com>
2023-04-30 18:49:54 -0400

    making a class to generate dataset

---
+++ src/main/java/org/yup/ml/MakeBelieveData.java
@@ -0,0 +1,10 @@
+package org.yup.ml;
+
+import java.util.List;
+import java.util.Map;
+
+public class MakeBelieveData {
+    private List<String> categories;
+    private Map<String, List<String>> vendors;
+    private Map<String, List<String>> descriptions;
+}
--- src/main/java/org/yup/ml/Reports.java
+++ src/main/java/org/yup/ml/Reports.java
@@ -1,4 +1,29 @@
 package org.yup.ml;
 
+import java.util.Date;
+
 public class Reports {
+    public void monthToDate(Ledger ledger) {
+
+    }
+
+    public void previousMonth(Ledger ledger) {
+
+    }
+
+    public void yearToDate(Ledger ledger) {
+
+    }
+
+    public void previousYear(Ledger ledger) {
+
+    }
+
+    public void searchByVendor(Ledger ledger, String vendor) {
+
+    }
+
+    public void customSearch(Ledger ledger, Date startDate, Date endDate, String description, String vendor, Double amount) {
+
+    }
 }
