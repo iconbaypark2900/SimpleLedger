14f1cb6af6a8f72f81071370b2af0ef5074f7fdf
iconbaypark2900 <jonaston015@gmail.com>
2023-05-05 00:35:09 -0400

    ohohoh,itsmagic

--- src/main/java/org/yup/ml/MakeBelieveData.java
+++ src/main/java/org/yup/ml/MakeBelieveData.java
@@ -14,17 +14,25 @@ import java.util.Random;
 public class MakeBelieveData {
 
     public static void main(String[] args) {
+        // Yeah found it on the internet and it started to work
         System.setProperty("weka.packageManager.offline", "true");
         System.setProperty("weka.core.welcome_disabled", "true");
 
+        // Examples to give the iffy bot some criteria
         String[] vendors = {"Amazon", "Walmart", "Best Buy", "ULine", "Target"};
         String[] descriptions = {"Electronics", "Groceries", "Clothing", "Office Supplies", "Home"};
         String[] categories = {"Shopping", "Groceries", "Apparel", "Work", "Home"};
         // create field [5] in order to show whether a transaction is a deposit or withdrawal
 
+        // Create A Balance for iffy bot
+        AccountBalance accountBalance = new AccountBalance(1_000_000);
+        accountBalance.loadTransactions();
+
+        // How many transactions I want; Creates a 1000 everytime class is ran
         int numberOfTransactions = 1000;
         String outputPath = "transaction.csv";
 
+        // Create output file just in case
         Path path = Paths.get(outputPath);
         try {
             if (!Files.exists(path)) {
@@ -34,6 +42,7 @@ public class MakeBelieveData {
             e.printStackTrace();
         }
 
+        // Write to csv
         try (BufferedWriter bw = new BufferedWriter(new FileWriter(outputPath, true))) {
             if (Files.size(path) == 0) {
                 bw.write("Date, Amount, Vendor, Description, Category\n");
@@ -55,25 +64,29 @@ public class MakeBelieveData {
             e.printStackTrace();
         }
 
+        // Magic
         MakeBelieveData makeBelieveData = new MakeBelieveData();
         makeBelieveData.trainAndEvaluate();
     }
 
+    // Generates random date
     private static LocalDate generateRandomDate() {
         Random random = new Random();
         int minYear = 2020;
-        int maxYear = 2022;
+        int maxYear = 2023;
         int year = random.nextInt(maxYear - minYear + 1) + minYear;
         int month = random.nextInt(12) + 1;
         int day = random.nextInt(28) + 1;
         return LocalDate.of(year, month, day);
     }
 
+    // Generate a random index
     private static int generateRandomIndex(int length) {
         Random random = new Random();
         return random.nextInt(length);
     }
 
+    // Generate a random amount between the specified range
     private static double generateRandomAmount() {
         Random random = new Random();
         double minAmount = -1000;
@@ -87,8 +100,9 @@ public class MakeBelieveData {
         return roundedAmount.doubleValue();
     }
 
+    // Magic pt.2
     public void trainAndEvaluate() {
         TransactionClassifier transactionClassifier = new TransactionClassifier();
-        transactionClassifier.trainAndEvaluateClassifier("transaction.csv", "trained_classifier.model");
+        transactionClassifier.trainAndEvaluateClassifier("transaction.csv", "trained_classifier1.model");
     }
 }
