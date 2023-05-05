51cf17663e4f85398dbf3159f637941784baf3c9
iconbaypark2900 <jonaston015@gmail.com>
2023-05-05 02:19:23 -0400

    payments & deposits

--- src/main/java/org/yup/ml/Reports.java
+++ src/main/java/org/yup/ml/Reports.java
@@ -6,16 +6,44 @@ import java.io.PrintWriter;
 import java.time.LocalDate;
 import java.time.YearMonth;
 import java.time.ZoneId;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 public class Reports {
+
+    // Display payments from the ledger
+    public void displayPayments(Ledger ledger) {
+        List<LedgerEntry> entries = ledger.getEntries();
+        List<LedgerEntry> payments = new ArrayList<>();
+        for(LedgerEntry entry : entries) {
+            if (entry.getAmount() < 0) {
+                payments.add(entry);
+            }
+        }
+        displayReport(payments);
+    }
+
+    // Display deposits from the ledger
+    public void displayDeposits(Ledger ledger) {
+        List<LedgerEntry> entries = ledger.getEntries();
+        List<LedgerEntry> deposits = new ArrayList<>();
+        for (LedgerEntry entry : entries) {
+            if (entry.getAmount() > 0) {
+                deposits.add(entry);
+            }
+        }
+        displayReport(deposits);
+    }
+
+    // Generate a report for the current month to date
     public void monthToDate(Ledger ledger) {
         LocalDate currentDate = LocalDate.now();
         LocalDate startOfMonth = currentDate.withDayOfMonth(1);
         generateReport(ledger, startOfMonth, currentDate);
     }
 
+    // Generate a report for the previous month
     public void previousMonth(Ledger ledger) {
         LocalDate currentDate = LocalDate.now();
         YearMonth lastMonth = YearMonth.from(currentDate).minusMonths(1);
@@ -24,12 +52,14 @@ public class Reports {
         generateReport(ledger, startOfMonth, endOfMonth);
     }
 
+    // Generate a report for the current year to date
     public void yearToDate(Ledger ledger) {
         LocalDate currentDate = LocalDate.now();
         LocalDate startOfYear = currentDate.withDayOfYear(1);
         generateReport(ledger, startOfYear, currentDate);
     }
 
+    // Generate a report for the previous year
     public void previousYear(Ledger ledger) {
         LocalDate currentDate = LocalDate.now();
         LocalDate startOfLastYear = currentDate.minusYears(1).withDayOfYear(1);
@@ -37,21 +67,25 @@ public class Reports {
         generateReport(ledger, startOfLastYear, endOfLastYear);
     }
 
+    // Generate a report with entries filtered by a specific vendor
     public void searchByVendor(Ledger ledger, String vendor) {
         List<LedgerEntry> filteredEntries = ledger.filterEntries(null, null, null, vendor, null);
         displayReport(filteredEntries);
     }
 
+    // Yeah somewhere over the rainbow
     public void customSearch(Ledger ledger, LocalDate startDate, LocalDate endDate, String description, String vendor, Double amount) {
         List<LedgerEntry> filteredEntries = ledger.filterEntries(convertToDate(startDate), convertToDate(endDate), description, vendor, amount);
         displayReport(filteredEntries);
     }
 
+    // Generate a report with entries filterd by the specified date range ex:
     private void generateReport(Ledger ledger, LocalDate startDate, LocalDate endDate) {
         List<LedgerEntry> filteredEntries = ledger.filterEntries(convertToDate(startDate), convertToDate(endDate), null, null, null);
         displayReport(filteredEntries);
     }
 
+    // Display the report
     private void displayReport(List<LedgerEntry> entries) {
         System.out.println("Date\t\tDescription\tVendor\tAmount");
         System.out.println("---------------------------------------------------");
@@ -60,6 +94,7 @@ public class Reports {
         }
     }
 
+    // Save report file
     public void saveReport(List<LedgerEntry> entries, String fileName) {
         try(PrintWriter writer = new PrintWriter(new FileWriter(fileName))) {
             writer.println("Date,Description,Vendor,Amount");
@@ -72,6 +107,7 @@ public class Reports {
         }
     }
 
+    // Convert a LocalDate obj
     private Date convertToDate(LocalDate localDate) {
         return Date.from(localDate.atStartOfDay(ZoneId.systemDefault()).toInstant());
     }
